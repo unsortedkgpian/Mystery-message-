09b89a8084b5d5830b73dbc7ba04567cf5efa4cd
unsortedkgpian <theadityakumar2810@gmail.com>
2025-02-15 11:34:08 +0530

    error fixing

--- src/app/api/verify-code/route.ts
+++ src/app/api/verify-code/route.ts
@@ -2,70 +2,63 @@ import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 
 export async function POST(request: Request) {
-	await dbConnect();
+    await dbConnect();
 
-	try {
-		const { username, code } = await request.json();
+    try {
+        const { username, code } = await request.json();
 
-		const decodeUsername = decodeURIComponent(username);
+        const decodeUsername = decodeURIComponent(username);
+        const usero = await UserModel.findOne({ username: decodeUsername });
 
-		const user = await UserModel.findOne({ username: decodeUsername });
+        if (!usero) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "User not found",
+                },
+                { status: 404 }
+            );
+        }
 
-		if (!user) {
-			return Response.json(
-				{
-					success: false,
-					message: "User not found",
-				},
-				{
-					status: 500,
-				}
-			);
-		}
+        const isCodeValid = usero.verifyCode === code;
+        const isCodeNotExpired = new Date(usero.verifyCodeExpiry) > new Date(); // Assuming verifyCodeExpiry field
 
-		const isCodeValid = user.verifyCode === code;
-		const isCodeNotExpired = new Date(user.verifyCode) > new Date();
-
-		if (isCodeNotExpired && isCodeValid) {
-			user.isVerified = true;
-			await user.save();
-			return Response.json(
-				{
-					success: true,
-					message: "Account verified successfully",
-				},
-				{
-					status: 200,
-				}
-			);
-		} else if (!isCodeNotExpired) {
-			return Response.json(
-				{
-					success: false,
-					message:
-						"Verification code has expired, please signup again to get a new code",
-				},
-				{ status: 400 }
-			);
-		} else {
-			return Response.json(
-				{
-					success: false,
-					message: "Incorrect Verification code",
-				},
-				{ status: 400 }
-			);
-		}
-	} catch (error) {
-		console.error("Error verifying user", error);
-		return Response.json(
-			{
-				success: false,
-				message: "Error verifying user",
-			},
-			{
-				status: 500,
-			}
-		);
-	}
+        if (isCodeNotExpired && isCodeValid) {
+            usero.isVerified = true;
+            await usero.save();
+            return Response.json(
+                {
+                    success: true,
+                    message: "Account verified successfully",
+                },
+                { status: 200 }
+            );
+        } else if (!isCodeNotExpired) {
+            return Response.json(
+                {
+                    success: false,
+                    message:
+                        "Verification code has expired, please sign up again to get a new code",
+                },
+                { status: 400 }
+            );
+        } else {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Incorrect Verification code",
+                },
+                { status: 400 }
+            );
+        }
+    } catch (error) {
+        console.error("Error verifying user", error);
+        return Response.json(
+            {
+                success: false,
+                message: "Error verifying user",
+            },
+            { status: 500 }
+        );
+    }
 }
--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,119 @@
 import NextAuth from "next-auth";
-import { authOptions } from "./app/api/auth/[...nextauth]/options";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+// import NextAuthOptions from "next-auth";
+// import { Session, JWT } from "next-auth";
+// import type { NextAuthConfig } from "next-auth";
+
+interface JWTToken {
+    _id?: string;
+    isAcceptingMessages?: boolean;
+    username?: string;
+    isVerified?: boolean;
+}
+
+interface Session {
+    user: {
+        _id?: string;
+        isVerified?: boolean;
+        isAcceptingMessages?: boolean;
+        username?: string;
+    };
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-	...authOptions,
-});
+    providers: [
+        CredentialsProvider({
+            id: "credentials",
+            name: "Credentials",
+            credentials: {
+                email: {
+                    label: "Email",
+                    type: "text",
+                },
+                password: { label: "Password", type: "password" },
+            },
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            async authorize(credentials: any): Promise<any> {
+                await dbConnect();
+                // trycatch for dbconnect
+                try {
+                    const user = await UserModel.findOne({
+                        $or: [
+                            { email: credentials.identifier },
+                            { username: credentials.identifier },
+                        ],
+                    });
+                    // user not found
+                    if (!user) {
+                        throw new Error("No user found with this email");
+                    }
+                    // user not verified
+                    if (!user.isVerified) {
+                        throw new Error(
+                            "Please verify you account before login"
+                        );
+                    }
+
+                    const isPasswordCorrect = await bcrypt.compare(
+                        credentials.password,
+                        user.password
+                    );
+
+                    if (isPasswordCorrect) {
+                        return user;
+                    } else {
+                        throw new Error("Password is incorrect");
+                    }
+                } catch (err) {
+                    throw new Error((err as Error).message);
+                }
+            },
+        }),
+    ],
+    // next auth will design ui in its onw
+    pages: {
+        signIn: "/sign-in",
+    },
+    session: {
+        strategy: "jwt",
+    },
+    secret: process.env.AUTH_SECRET,
+    callbacks: {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        async jwt({ token, user }: { token: JWTToken; user: User | null }) {
+            // it will give error so for it we use coustom type declare in
+            // types-> next auth
+            if (user) {
+                token._id = user._id?.toString();
+                token.isAcceptingMessages = user.isAcceptingMessages;
+                token.username = user.username;
+                token.isVerified = user.isVerified;
+            }
+
+            return token;
+        },
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        async session({
+            session,
+            token,
+        }: {
+            session: Session;
+            token: JWTToken;
+        }) {
+            if (token) {
+                session.user._id = token._id as string;
+                session.user.isVerified = token.isVerified as boolean;
+                session.user.isAcceptingMessages =
+                    token.isAcceptingMessages as boolean;
+                session.user.username = token.username as string;
+            }
+
+            return session;
+        },
+    },
+})
